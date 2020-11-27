3a819fffc3cf2ca837ef017d08a7878e42badaf6
Adrian Keil <adrian.keil@tum.de>
2020-11-27 10:04:48 +0100

    Outsourced drawing to a separate task

--- src/main.c
+++ src/main.c
@@ -31,6 +31,10 @@
 #define TEXT_OFFSET_Y       150
 
 static TaskHandle_t DemoTask = NULL; //Init with NULL, so you can check if it has been initialized
+static TaskHandle_t BufferSwap = NULL;
+
+static SemaphoreHandle_t DrawSignal = NULL;
+static SemaphoreHandle_t ScreenLock = NULL;
 
 typedef struct buttons_buffer {
     unsigned char buttons[SDL_NUM_SCANCODES];
@@ -101,99 +105,111 @@ int getTriangleCoordinates(coord_t *coordinates, int x, int y, int width, int he
     return 0;
 }
 
+void vSwapBuffers(void *pvParameters)
+{
+    TickType_t xLastWakeTime;
+    xLastWakeTime = xTaskGetTickCount();
+    const TickType_t frameratePeriod = 20;
+
+    tumDrawBindThread(); // Setup Rendering handle with correct GL context
+
+    while (1) {
+        if (xSemaphoreTake(ScreenLock, portMAX_DELAY) == pdTRUE) {
+            tumDrawUpdateScreen();
+            tumEventFetchEvents(FETCH_EVENT_BLOCK);
+            xSemaphoreGive(ScreenLock);
+            xSemaphoreGive(DrawSignal);
+            vTaskDelayUntil(&xLastWakeTime,
+                            pdMS_TO_TICKS(frameratePeriod));
+        }
+    }
+}
+
 void vDemoTask(void *pvParameters)
 {
-    /* ######## Initialization ############## */
-    // structure to store my text
-    static char my_string[100];
+    static char my_string[100]; // structure to store my text
     static int my_string_width = 0;
     static coord_t mycoordinates[4];
     static float i=0;
     static int offset_x;
     static int offset_y;
 
-
-    // Needed such that Gfx library knows which thread controlls drawing
-    // Only one thread can call tumDrawUpdateScreen while and thread can call
-    // the drawing functions to draw objects. This is a limitation of the SDL
-    // backend.
-    tumDrawBindThread();
-
-    /* ############# Actual program ################ */
     while (1) {
-        tumEventFetchEvents(FETCH_EVENT_NONBLOCK); // Query events backend for new events, ie. button presses
-        xGetButtonInput(); // Update global input
+        if (DrawSignal) {
+            if (xSemaphoreTake(DrawSignal, portMAX_DELAY) == pdTRUE) {
+                xGetButtonInput(); // Update global input
 
-        // `buttons` is a global shared variable and as such needs to be
-        // guarded with a mutex, mutex must be obtained before accessing the
-        // resource and given back when you're finished. If the mutex is not
-        // given back then no other task can access the reseource.
-        if (xSemaphoreTake(buttons.lock, 0) == pdTRUE) {
-            if (buttons.buttons[KEYCODE(
-                                    Q)]) { // Equiv to SDL_SCANCODE_Q
-                exit(EXIT_SUCCESS);
+                // `buttons` is a global shared variable and as such needs to be
+                // guarded with a mutex, mutex must be obtained before accessing the
+                // resource and given back when you're finished. If the mutex is not
+                // given back then no other task can access the reseource.
+                if (xSemaphoreTake(buttons.lock, 0) == pdTRUE) {
+                    if (buttons.buttons[KEYCODE(
+                                            Q)]) { // Equiv to SDL_SCANCODE_Q
+                        exit(EXIT_SUCCESS);
+                    }
+                    xSemaphoreGive(buttons.lock);
+                }
+
+                xSemaphoreTake(ScreenLock, portMAX_DELAY);
+                
+                tumDrawClear(White); // Clear screen
+
+                //Calculate offset for rotating parts
+                offset_x = (int) (ROTATION_RADIUS * cos(i));
+                offset_y = (int) (ROTATION_RADIUS * sin(i));
+        
+                //Draw the Circle
+                //tumDrawArc is unsed instead of tumDrawCircle to create just the outline and not a filled circle
+                tumDrawArc(  SCREEN_WIDTH / 2 - offset_x, 
+                                SCREEN_HEIGHT / 2 + offset_y,
+                                MYCIRCLE_RADIUS,
+                                0,
+                                359,
+                                Purple);
+
+                //Create the points for the triangle
+                getTriangleCoordinates(mycoordinates, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, TRIANGLE_WIDTH, TRIANGLE_HEIGHT);
+
+                tumDrawPoly(mycoordinates, 3, Teal);  //Draw the triangle
+
+
+                //Create the points for the square
+                getSquareCorrdinates(mycoordinates, SCREEN_WIDTH / 2 + offset_x, SCREEN_HEIGHT / 2 - offset_y, SQUARE_LENGTH);
+
+                tumDrawPoly(mycoordinates, 4, Fuchsia);  //Draw the sqare
+
+                // Format the string into the char array
+                sprintf(my_string, "Hello ESPL");
+                // Get the width of the string on the screen so we can center it
+                // Returns 0 if width was successfully obtained
+                if (!tumGetTextSize((char *)my_string, &my_string_width, NULL))
+                    tumDrawText(my_string,
+                                SCREEN_WIDTH / 2 - my_string_width / 2,
+                                SCREEN_HEIGHT / 2 - DEFAULT_FONT_SIZE / 2 + TEXT_OFFSET_Y,
+                                TUMBlue);
+                
+                
+                sprintf(my_string, "This is exercise 2.1");
+                // Get the width of the string on the screen so it can be centered
+                if (!tumGetTextSize((char *)my_string, &my_string_width, NULL))
+                {
+                    //Calculate the offset for the text (offset_x is reused here)
+                    offset_x = (int) (offset_x * (SCREEN_WIDTH / 2 - my_string_width / 2) / ROTATION_RADIUS); 
+                    tumDrawText(my_string,
+                                SCREEN_WIDTH / 2 - my_string_width / 2 + offset_x,
+                                SCREEN_HEIGHT / 2 - DEFAULT_FONT_SIZE / 2 - TEXT_OFFSET_Y,
+                                TUMBlue);
+                }
+
+                xSemaphoreGive(ScreenLock);
+
+                if(i >= 2*M_PI)
+                    i=0;
+                else
+                    i += 0.02;
             }
-            xSemaphoreGive(buttons.lock);
         }
-
-        tumDrawClear(White); // Clear screen
-
-        //Calculate offset for rotating parts
-        offset_x = (int) (ROTATION_RADIUS * cos(i));
-        offset_y = (int) (ROTATION_RADIUS * sin(i));
- 
-        //Draw the Circle 
-        //tumDrawArc is unsed instead of tumDrawCircle to create just the outline and not a filled circle
-        tumDrawArc(  SCREEN_WIDTH / 2 - offset_x, 
-                        SCREEN_HEIGHT / 2 + offset_y,
-		                MYCIRCLE_RADIUS,
-                        0,
-                        359,
-	                    Purple);
-
-        //Create the points for the triangle
-        getTriangleCoordinates(mycoordinates, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, TRIANGLE_WIDTH, TRIANGLE_HEIGHT);
-
-        tumDrawPoly(mycoordinates, 3, Teal);  //Draw the triangle
-
-
-        //Create the points for the square
-        getSquareCorrdinates(mycoordinates, SCREEN_WIDTH / 2 + offset_x, SCREEN_HEIGHT / 2 - offset_y, SQUARE_LENGTH);
-
-        tumDrawPoly(mycoordinates, 4, Fuchsia);  //Draw the sqare
-
-        // Format the string into the char array
-        sprintf(my_string, "Hello ESPL");
-        // Get the width of the string on the screen so we can center it
-        // Returns 0 if width was successfully obtained
-        if (!tumGetTextSize((char *)my_string, &my_string_width, NULL))
-            tumDrawText(my_string,
-                        SCREEN_WIDTH / 2 - my_string_width / 2,
-                        SCREEN_HEIGHT / 2 - DEFAULT_FONT_SIZE / 2 + TEXT_OFFSET_Y,
-                        TUMBlue);
-        
-        
-        sprintf(my_string, "This is exercise 2.1");
-        // Get the width of the string on the screen so it can be centered
-        if (!tumGetTextSize((char *)my_string, &my_string_width, NULL))
-        {
-            //Calculate the offset for the text (offset_x is reused here)
-            offset_x = (int) (offset_x * (SCREEN_WIDTH / 2 - my_string_width / 2) / ROTATION_RADIUS); 
-            tumDrawText(my_string,
-                        SCREEN_WIDTH / 2 - my_string_width / 2 + offset_x,
-                        SCREEN_HEIGHT / 2 - DEFAULT_FONT_SIZE / 2 - TEXT_OFFSET_Y,
-                        TUMBlue);
-        }
-
-        tumDrawUpdateScreen(); // Refresh the screen to draw string
-
-        if(i >= 2*M_PI)
-            i=0;
-        else
-            i += 0.02;
-        
-
-        vTaskDelay((TickType_t)1);
     }
 }
 
@@ -226,7 +242,23 @@ int main(int argc, char *argv[])
         goto err_buttons_lock;
     }
 
-    /* Here You can start modifying  */
+    DrawSignal = xSemaphoreCreateBinary(); // Screen buffer locking
+    if (!DrawSignal) {
+        PRINT_ERROR("Failed to create draw signal");
+        goto err_draw_signal;
+    }
+    ScreenLock = xSemaphoreCreateMutex();
+    if (!ScreenLock) {
+        PRINT_ERROR("Failed to create screen lock");
+        goto err_screen_lock;
+    }
+
+    if (xTaskCreate(vSwapBuffers, "BufferSwapTask",
+                    mainGENERIC_STACK_SIZE * 2, NULL, configMAX_PRIORITIES,
+                    BufferSwap) != pdPASS) {
+        goto err_bufferswap;
+    }
+
     if (xTaskCreate(vDemoTask, "DemoTask", mainGENERIC_STACK_SIZE * 2, NULL,
                     mainGENERIC_PRIORITY, &DemoTask) != pdPASS) {
         goto err_demotask;
@@ -237,6 +269,12 @@ int main(int argc, char *argv[])
     return EXIT_SUCCESS;
 
 err_demotask:
+    vTaskDelete(BufferSwap);
+err_bufferswap:
+    vSemaphoreDelete(ScreenLock);
+err_screen_lock:
+    vSemaphoreDelete(DrawSignal);
+err_draw_signal:
     vSemaphoreDelete(buttons.lock);
 err_buttons_lock:
     tumSoundExit();
